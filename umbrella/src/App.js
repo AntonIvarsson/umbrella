a1d0a0c6628de0914556529fd7243262bed2fe01
Anton Ivarsson <anton.ivarsson@allabrf.se>
2018-04-16 20:56:32 +0200

    Added random complimen and buss-times for sockenplan.

--- umbrella/src/App.js
+++ umbrella/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Components/Header.js'
+import Footer from './Components/Footer.js'
 import './Assets/Stylesheets/App.css';
+import TimesContainer from './Components/TimesContainer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <TimesContainer />
+        <Footer />
       </div>
     );
   }
--- umbrella/src/Components/Header.js
+++ umbrella/src/Components/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Clock from 'react-live-clock';
 import axios from 'axios';
+import TimesContainer from './TimesContainer.js';
 
 import '../Assets/Stylesheets/App.css';
 
-const weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=9743140833919d56c2c140b9c34016f5&units=metric';
+const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=9743140833919d56c2c140b9c34016f5&units=metric';
 
 class Header extends Component {
 
@@ -15,14 +16,18 @@ class Header extends Component {
       temp: 'Fetching weather <--> '
     };
   }
-  
+
   componentWillMount(){
     this.getWeather();
+    setInterval(() => {
+      this.getWeather();
+    },600000);
   }
 
   async getWeather() {
     try {
-      const response = await axios.get(weatherUrl);
+      const response = await axios.get(URL);
+      console.log(response.data)
       this.setState({ temp: Math.round(response.data.main.temp)})
     } catch (error) {
       console.error(error);
@@ -32,11 +37,13 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <header className="App-header">
-          <Clock format={'HH:mm:ss'} ticking={true} timezone={'Europe/Stockholm'} />
+        <header className='App-header'>
+          <p> Mot TC: </p>
+          <Clock format={'HH:mm'} ticking={true} timezone={'Europe/Stockholm'} />
           <div className='Header__temperature__gif--group'>
           <p> {this.state.temp} °C </p>
-          <img src={'https://thumbs.gfycat.com/SharpCalculatingIslandwhistler-size_restricted.gif'} className='App__gif--rain'/>
+          <img className = 'App__gif--rain'
+          src={'https://static1.squarespace.com/static/5394702fe4b092dc8ae3d015/539c77b2e4b05219d5bd8b95/53b1805fe4b0732f8676875d/1404141706865/cloud.gifformat=1000w'}/> 
           </div>
         </header>
       </div>
